8c638eb1187328a67d71605a73de21d112341aca
TanyaNikonenko <tanya.nikonenko@mail.ru>
2020-07-01 11:33:47 +0300

    release v1

---
+++ main.py
@@ -0,0 +1,68 @@
+conn = sqlite3.connect('rosterdb.sqlite')
+cur = conn.cursor()
+
+cur.executescript('''
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS Course;
+
+CREATE TABLE User (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name   TEXT UNIQUE
+);
+
+CREATE TABLE Course (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title  TEXT UNIQUE
+);
+
+CREATE TABLE Member (
+    user_id     INTEGER,
+    course_id   INTEGER,
+    role        INTEGER,
+    PRIMARY KEY (user_id, course_id)
+)
+''')
+
+fname = input('Enter file name: ')
+if len(fname) < 1:
+    fname = 'roster_data_sample.json'
+
+str_data = open(fname).read()
+json_data = json.loads(str_data)
+
+for entry in json_data:
+    name = entry[0];
+    title = entry[1];
+
+    print((name, title))
+
+    cur.execute('''INSERT OR IGNORE INTO User (name)
+        VALUES ( ? )''', ( name, ) )
+    cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
+    user_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Course (title)
+        VALUES ( ? )''', ( title, ) )
+    cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
+    course_id = cur.fetchone()[0]
+
+    if user_id == course_id:
+        cur.execute('''INSERT OR REPLACE INTO Member
+            (user_id, course_id,role) VALUES ( ?, ? ,?)''',
+            ( user_id, course_id ,'1') )
+    else:
+        cur.execute('''INSERT OR REPLACE INTO Member
+            (user_id, course_id,role) VALUES ( ?, ? ,?)''',
+            ( user_id, course_id ,'0') )
+    conn.commit()
+
+sqlstr = '''
+SELECT hex(User.name || Course.title || Member.role ) AS X FROM
+    User JOIN Member JOIN Course
+    ON User.id = Member.user_id AND Member.course_id = Course.id
+    ORDER BY X'''
+for row in cur.execute(sqlstr):
+    print(str(row[0]))
+
+cur.close()
